040c7d503b37b60a8986dcd72a434f0c98c997f0
chtello <112952907+chtello@users.noreply.github.com>
2023-04-18 13:06:52 -0300

    Add files via upload

---
+++ -trabajoCALCULADORA-main/TP.Calcuadora/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementsByClassName('borr');
+
+btn.addEventListener("click", () => {
+  console.log("hola");
+  document.addEventListener("form-control").innertext = "";
+}); 
+
